8abbf7835f9b470a96a251133e542e2f6ca14191
cexbrayat <cedric@ninja-squad.com>
2014-05-27 21:43:04 +0200

    chore: run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 # Kill old container
 echo "kill old container"
-sudo docker kill $(cat docker.pid)
+docker kill $(cat docker.pid)
 
 # Build new image
 echo "build image"
-sudo docker build -t ninjasquad/tp-ng .
+docker build -t ninjasquad/tp-ng .
 
 # Run image
 echo "run image"
-DOCKER_CONTAINER=$(sudo docker run -p 80:80 -d ninjasquad/tp-ng)
+DOCKER_CONTAINER=$(docker run -p 80:80 -d ninjasquad/tp-ng)
 
 # Save container id
 rm docker.pid
